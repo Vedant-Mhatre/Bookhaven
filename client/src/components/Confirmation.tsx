27806df11ef389ece9250a5d94eb50aa6eb054b3
Vedant-Mhatre <vedantmh@gmail.com>
2024-05-07 20:48:59 -0400

    css

--- client/src/components/Confirmation.tsx
+++ client/src/components/Confirmation.tsx
@@ -1,14 +1,69 @@
+import { useContext } from "react";
 import "../assets/css/confirmation.css";
+import { Link, useNavigate } from "react-router-dom";
+import ConfirmationTable from "./ConfirmationTable";
+import { OrderStore } from "../contexts/OrderDetailContext";
 
-function Confirmation() {
-
+const Cart = () => {
+  const { orderDetails } = useContext(OrderStore);
+  const navigate = useNavigate();
   return (
-    <section className="confirmation">
-      <h1>Order Placed!</h1>
-      <h2>Thank you for shopping with us!</h2>
-    </section>
+    <div>
+
+      {!orderDetails || !orderDetails.order ? (
+        <>
+          <div className="conf-center-text">
+            <p>We are sorry, the order you requested could not be found.</p>
+          </div>
+          <div className="conf-center-text">
+            <button className="conf-button" onClick={() => navigate("/")}>
+              Go to Home Page
+            </button>
+          </div>
+        </>
+      ) : (
+        <div className="conf-center-text">
+          <div className="confirmation-text">
+            <ul className="conf-ul">
+                <li className="conf-li">
+                Your confirmation number is{" "}
+                {orderDetails.order.confirmationNumber}
+                </li>
+                <br />
+                <li className="conf-li">
+                Date created:{" "}
+                {new Date(orderDetails.order.dateCreated).toString()}
+                </li>
+                <br />
+                <li className="conf-li">
+                Customer Name: {orderDetails.customer.customerName}
+                </li>
+                <br />
+                <li className="conf-li">Email: {orderDetails.customer.email}</li>
+                <br />
+                <li className="conf-li">
+                Address: {orderDetails.customer.address}
+                </li>
+                <br />
+                <li className="conf-li">Phone: {orderDetails.customer.phone}</li>
+                <br />
+                </ul>
+                </div>
+
+            <ConfirmationTable />
+            <div className="cart-actions">
+              <Link to={`/categories/New-Releases`}>
+                <button className="confirmation-shopping-button">
+                  Continue Shopping
+                </button>
+              </Link>
+            </div>
+          
+        </div>
+      )}
+      <br />
+    </div>
   );
-}
+};
 
-
-export default Confirmation;
+export default Cart;
